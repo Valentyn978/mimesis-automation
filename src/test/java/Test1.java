8d025ac7ac2c1bd6cac4be0d025ee8a889aae765
valentyn.yanchii <valentyn.yanchii@globallogic.com>
2017-11-24 18:06:01 +0200

    Add junit tests

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,17 +1,24 @@
+import com.helpers.SetRequestHeaders;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class Test1 {
 
+    private SetRequestHeaders setHeaders = new SetRequestHeaders();
+
     @Test
     public void testMethod1(){
         System.out.printf("Test Method1; Class1");
-        Assert.assertTrue(1 == 1);
+        Object ob = setHeaders.setHeaders("http://test.com", "POST");
+
+        Assert.assertEquals(ob.getClass().getName(), "org.apache.http.client.methods.HttpPost");
     }
 
     @Test
     public void testMethod2(){
         System.out.printf("Test Method2; Class1");
-        Assert.assertTrue(1 == 1);
+        Object ob = setHeaders.setHeaders("http://test.com", "PATCH");
+
+        Assert.assertEquals(ob.getClass().getName(), "org.apache.http.client.methods.HttpPatch");
     }
 }
