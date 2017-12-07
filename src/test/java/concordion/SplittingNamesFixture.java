a6d560e949cd58b60d5c30ad0d808b9310fe7e20
valentyn.yanchii <valentyn.yanchii@globallogic.com>
2017-12-07 10:07:51 +0200

    ass concordion FW classes and gradle

---
+++ src/test/java/concordion/SplittingNamesFixture.java
@@ -0,0 +1,21 @@
+package concordion;
+
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(ConcordionRunner.class)
+public class SplittingNamesFixture {
+
+    public Result split(String fullName) {
+        Result result = new Result();
+        String[] words = fullName.split(" ");
+        result.firstName = words[0];
+        result.lastName = words[1];
+        return result;
+    }
+
+    class Result {
+        public String firstName;
+        public String lastName;
+    }
+}
